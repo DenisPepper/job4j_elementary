871eaf147cff951dc8be66dfcd026cf88e9ad519
DenisPepper <denispepper>
2020-03-23 20:26:27 +0300

    9.1. String[#259392]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+import java.util.Calendar;
+
+
+
+public class Greeting {
+
+    public static void main(String[] args) {
+
+        String idea = "I like Java!";
+        System.out.println(idea);
+
+        idea += "But I am a newbie.";
+
+        Calendar date = Calendar.getInstance();
+        int year = date.get(Calendar.YEAR);
+
+        idea += year;
+
+        System.out.println(idea);
+    }
+
+}
