09ea902d88fead7512b0bf1661dfc49d396b0664
DenisPepper <denispepper>
2020-03-28 21:46:18 +0300

    4.2. Тернарное сравнение[#259350]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -3,7 +3,6 @@ package ru.job4j.condition;
 public class Max {
     public static int max(int first, int second) {
 
-        int spread = first - second;
-        return spread >= 0 ? first : second;
+        return first >= second ? first : second;
     }
 }
