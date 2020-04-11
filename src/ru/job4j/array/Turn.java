6c35cb226b76d620751b5ffe66d8dc73753d5ce9
DenisPepper <denispepper>
2020-04-11 17:17:39 +0300

    6.2.3. Перевернуть массив.[#259373], // Достаточно организовать цикл до середины. Тогда эти условия не нужны будут

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -3,13 +3,13 @@ package ru.job4j.array;
 public class Turn {
 
     public static int[] back(int[] array) {
-        for (int index = 0; index < array.length; index++) {
-            if (array.length % 2 == 0 && index == array.length / 2) {
-                break;
-            }
-            if (array.length % 2 != 0 && index == (array.length - 1) / 2) {
-                break;
-            }
+        for (int index = 0; index < array.length / 2; index++) {
+            //if (array.length % 2 == 0 && index == array.length / 2) {
+            //    break;
+            //}
+           // if (array.length % 2 != 0 && index == (array.length - 1) / 2) {
+            //    break;
+            //}
             int remove = array[index];
             array[index] = array[array.length - index - 1];
             array[array.length - index - 1] = remove;
