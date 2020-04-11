0ed51d12c7803cba48148d8c4cdbc42e321611de
DenisPepper <denispepper>
2020-04-11 18:33:44 +0300

    6.3. Массив заполнен true или false[#259359]

---
+++ src/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        if (pref.length > word.length) {
+            result = false;
+        } else {
+            for (int index = 0; index < pref.length; index++) {
+                if (pref[index] != word[index]) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
