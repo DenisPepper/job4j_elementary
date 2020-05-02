44efb599619c36793cc9598505d9ff16d4387dc2
DenisPepper <denispepper>
2020-05-02 11:08:23 +0300

    3. Перегрузить метод max для трех чисел [#259511] - рефакторинг

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -3,14 +3,15 @@ package ru.job4j.condition;
 public class Max {
 
     public static int max(int first, int second) {
+
         return first >= second ? first : second;
     }
 
     public static int max(int first, int second, int third) {
-        return first >= max(second, third) ? first : max(second, third);
+        return max(first, max(second, third));
     }
 
     public static int max(int first, int second, int third, int fourth) {
-        return first >= max(second, max(third, fourth)) ? first : max(second, max(third, fourth));
+        return max(first, max(second, max(third, fourth)));
     }
 }
